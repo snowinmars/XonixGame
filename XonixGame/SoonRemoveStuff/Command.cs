6905ab4a06e34b2b98b0439878fbf934ed334bab
Dmitrii_Tkachenko <Dmitrii_Tkachenko@epam.com>
2016-07-12 16:54:59 +0300

    Created Command pattern class

---
+++ XonixGame/SoonRemoveStuff/Command.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoonRemoveStuff
+{
+    public class Command
+    {
+        public Command()
+        {
+            
+        }
+
+        public Command(Action action)
+        {
+            this.Action = action;
+        }
+
+        public Action Action { get; set; }
+
+        public void Exec()
+        {
+            this.Action?.Invoke();
+        }
+    }
+}
