b8d14217e417a563d94fd573ef35b725a85b0ea5
Dmitrii_Tkachenko <Dmitrii_Tkachenko@epam.com>
2016-09-29 14:30:27 +0300

    Refactored again.

--- XonixGame/XonixGame.Monogame/XonixGame.cs
+++ XonixGame/XonixGame.Monogame/XonixGame.cs
@@ -6,9 +6,6 @@ using XonixGame.Entities;
 
 namespace XonixGame.Monogame
 {
-    /// <summary>
-    /// This is the main type for your game.
-    /// </summary>
     public class XonixGame : Game
     {
         private GraphicsDeviceManager graphics;
@@ -27,15 +24,9 @@ namespace XonixGame.Monogame
             this.IsMouseVisible = true;
         }
 
-        /// <summary>
-        /// This is called when the game should draw itself.
-        /// </summary>
-        /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Draw(GameTime gameTime)
         {
             this.GraphicsDevice.Clear(Color.CornflowerBlue);
-
-            // Add your drawing code here
 
             this.spriteBatch.Begin();
 
@@ -46,22 +37,15 @@ namespace XonixGame.Monogame
             base.Draw(gameTime);
         }
 
-        /// <summary>
-        /// Allows the game to perform any initialization it needs to before starting to run.
-        /// This is where it can query for any required services and load any non-graphic
-        /// related content.  Calling base.Initialize will enumerate through any components
-        /// and initialize them as well.
-        /// </summary>
         protected override void Initialize()
         {
-            // Add your initialization logic here
-
             TextureStorage.Initialize(this.Content, this.GraphicsDevice);
             FontStorage.Initialize(this.Content);
 
-            RasterizerState rasterizerState = new RasterizerState();
-            rasterizerState.CullMode = CullMode.None;
-            this.GraphicsDevice.RasterizerState = rasterizerState;
+            this.GraphicsDevice.RasterizerState = new RasterizerState
+            {
+                CullMode = CullMode.None
+            };
 
             Player player = new Player(0, 0);
             this.world = new GameWorld(player);
@@ -70,34 +54,17 @@ namespace XonixGame.Monogame
             base.Initialize();
         }
 
-        /// <summary>
-        /// LoadContent will be called once per game and is the place to load
-        /// all of your content.
-        /// </summary>
         protected override void LoadContent()
         {
-            // Create a new SpriteBatch, which can be used to draw textures.
             this.spriteBatch = new SpriteBatch(this.GraphicsDevice);
 
             this.world.LoadContent(this.Content, this.GraphicsDevice);
-
-            // use this.Content to load your game content here
         }
 
-        /// <summary>
-        /// UnloadContent will be called once per game and is the place to unload
-        /// game-specific content.
-        /// </summary>
         protected override void UnloadContent()
         {
-            // Unload any non ContentManager content here
         }
 
-        /// <summary>
-        /// Allows the game to run logic such as updating the world,
-        /// checking for collisions, gathering input, and playing audio.
-        /// </summary>
-        /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Update(GameTime gameTime)
         {
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed ||
@@ -105,8 +72,6 @@ namespace XonixGame.Monogame
             {
                 this.Exit();
             }
-
-            // Add your update logic here
 
             this.world.Update();
 
